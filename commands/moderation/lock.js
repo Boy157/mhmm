9eca8fefcc948bf9f114ad82b4740ab407e58a56
Glitch (aiuwhdiuawhdiuawduhaw) <none>
2021-04-03 13:07:57 +0000

    🚗🏋 Checkpoint ./commands/info/help.js:25955209/11 ./commands/moderation/lock.js:25955209/777

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -15,18 +15,12 @@ module.exports = {
       );
     }
     
-    message.channel.overwritePermissions([
-      {
-        id: message.guild.id,
-        deny: ["SEND_MESSAGES"]
-      }
-    ]);
+    const role = message.guild.roles.cache.get('777773709997899836');
+    let lockChannel = message.mention.channels.first() || message.guild.channels.cache(args[0]);
+    if (!lockChannel) lockChannel = message.channel;
     
-    const embed = new Discord.MessageEmbed()
-    
-      .setDescription(`:lock: Locked down ${message.channel}`)
-      .setColor("BLUE");
-    await message.channel.send(embed);
-    
+    await lockChannel.updateOverwrites(role, {
+      SEND_MESSAGES: false
+    }).catch(err => console.log(err));
   }
-};
+}
