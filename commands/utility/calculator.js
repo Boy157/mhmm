69cfe05a7def362acf85e15edace3c1fcf480bac
Glitch (aiuwhdiuawhdiuawduhaw) <none>
2021-04-03 14:17:57 +0000

    🍨🙏 Checkpoint ./commands/utility/calculator.js:25955209/590 ./commands/utility/snipe.js:25955209/745

--- commands/utility/calculator.js
+++ commands/utility/calculator.js
@@ -13,29 +13,29 @@ module.exports = {
     const secondValue = Number(args[2]);
     
     if (!args[0]) return message.channel.send(`You have to input an arguments, Example: 1 + 1`);
-    if (!firstValue) return message.channel.send('The first value stated is not a number');
+    if (!firstValue) return message.channel.send('The first value stated is not a number or u need to give space in the middle.');
     if (!args[1]) return message.channel.send('You have to state what you want to do with the number. Here is an option: +, -, *, /');
     if (!['+', '-', '*', '/'].includes(args[1])) return message.channel.send('You have to use the variable: +, -, *, /');
-    if (!secondValue) return message.channel.send(`The first value stated is not a number`);
+    if (!secondValue) return message.channel.send('The second value stated is not a number or u need to give space in the middle');
     
     if (args[1] == '+') {
       let result = firstValue + secondValue;
-      message.channel.send(`${fisrtValue} + ${secondValue} = ${result}.`);
+      message.channel.send(`${firstValue} + ${secondValue} = ${result}.`);
     }
     
     if (args[1] == '-') {
       let result = firstValue - secondValue;
-      message.channel.send(`${fisrtValue} - ${secondValue} = ${result}.`);
+      message.channel.send(`${firstValue} - ${secondValue} = ${result}.`);
     }
     
     if (args[1] == '*') {
-      let result = firstValue - secondValue;
-      message.channel.send(`${fisrtValue} * ${secondValue} = ${result}.`);
+      let result = firstValue * secondValue;
+      message.channel.send(`${firstValue} x ${secondValue} = ${result}.`);
     }
     
     if (args[1] == '/') {
       let result = firstValue / secondValue;
-      message.channel.send(`${fisrtValue} / ${secondValue} = ${result}.`);
+      message.channel.send(`${firstValue} : ${secondValue} = **${result}**.`);
     }
   }
 }
