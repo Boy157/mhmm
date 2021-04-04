db4fa9389aa4339668dc55d08bd4bf7736b5573a
Glitch (aiuwhdiuawhdiuawduhaw) <none>
2021-04-04 11:58:31 +0000

    ✂️🌘 Checkpoint ./commands/utility/math.js:25955209/1992 ./package.json:25955209/2082

--- commands/utility/math.js
+++ commands/utility/math.js
@@ -10,19 +10,16 @@ module.exports = {
     //Start
     const firstValue = Number(args[0]);
     const secondValue = Number(args[2]);
-    const thirdValue = Number(args[4]);
     
     if (!args[0]) return message.channel.send(`You have to input an arguments, Example: 1 + 1`);
     if (!firstValue) return message.channel.send('The first value stated is not a number or u need to give space in the middle.');
     if (!args[1]) return message.channel.send('You have to state what you want to do with the number. Here is an option: +, -, *, /');
     if (!['+', '-', '*', '/'].includes(args[1])) return message.channel.send('You have to use the variable: +, -, *, /');
     if (!secondValue) return message.channel.send('The second value stated is not a number or u need to give space in the middle');
-    if (!thirdValue) return message.channel.send('The third value stated is not a number or u need to give space in the middle');
     
     if (args[1] == '+') {
       let result = firstValue + secondValue;
-      let result = firstValue + secondValue + thirdValue;
-      message.channel.send(`${firstValue} + ${secondValue} + ${thirdValue} = ${result}.`);
+      message.channel.send(`${firstValue} + ${secondValue} = ${result}.`);
     }
     
     if (args[1] == '-') {
