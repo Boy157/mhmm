aad3fc36681951c62ca6631dd0761b282517cb94
Glitch (aiuwhdiuawhdiuawduhaw) <none>
2021-04-04 06:44:33 +0000

    🗽⛲️ Checkpoint ./commands/moderation/mute.js:25955209/1092

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 const { Color } = require("../../config.js");
-const ms = require('ms');
+const ms = require("ms");
 
 module.exports = {
   name: "mute",
@@ -16,20 +16,20 @@ module.exports = {
         `You Don't Have Permission To Use This Command!`
       );
 
-    let muteUser =
+    let Member =
       message.mentions.members.first() ||
       message.guild.members.cache.get(args[0]);
 
-    if (!muteUser) return message.channel.send(`Please Mention A User!`);
+    if (!Member) return message.channel.send(`Please Mention A User!`);
 
-    let MuteRole = message.guild.roles.cache.find(role => role.name === "Muted").id;
+    let Role = message.guild.roles.cache.find(role => role.name === "Muted").id;
 
-    if (!MuteRole)
+    if (!Role)
       return message.channel.send(
         `Please Create Mute Role | Role Name : Muted`
       );
 
-    if (muteUser.roles.cache.has(Role)) {
+    if (Member.roles.cache.has(Role)) {
       return message.channel.send(`Member Is Already Muted!`);
     }
     let time = args[1];
@@ -38,26 +38,18 @@ module.exports = {
 
     if (!User.bannable) return message.channel.send(`I Can't Mute That Member!`);
     
-    if(!muteUser.roles.highest.position >= message.member.roles.highest.position) return message.chanel.send('You cant mute a member that higher than you')
+    if(!Member.roles.highest.position >= message.member.roles.highest.position) return message.chanel.send('You cant mute a member that higher than you')
 
     let Embed = new MessageEmbed()
       .setColor("RANDOM")
       .setTitle(`Member Muted!`)
-      .addField(`Moderator`, `${message.author.tag}`)
+      .addField(`Moderator`, `**${message.author.tag}`)
       .addField(`Muted Member`, `${Member.user.tag}`)
       .addField(`Reason`, `${Reason || "No Reason Provided!"}`)
-      .addField(`Duration`, `${time}`)
+      .addField(`Duration`, `${time || "No Time selected!"}`)
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
     
-    
-  	timeout(minutes, muteUser, muteRole, message)
-
-    function timeout(minutes, muteUser, mutedRole, message) {
-      setTimeout(async function () {
-        muteUser.roles.remove(mutedRole);
-              
-    }, ms (time));
 
     if (Role && !Member.roles.cache.has(Role)) {
       Member.roles.add([Role]);
@@ -65,3 +57,7 @@ module.exports = {
     } else {
       return message.channel.send(`Something Went Wrong, Try Again Later!`);
     }
+
+    //End
+  }
+};
